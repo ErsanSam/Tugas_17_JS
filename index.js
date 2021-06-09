66a53136480130b77c96bd5f245c88b23aa4d1b4
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-09 16:11:20 +0700

    Tugas 17 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,6 @@
+var huruf = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+console.log(huruf.toLowerCase())
+console.log(huruf.toUpperCase())
+console.log(huruf2.toLowerCase())
+console.log(huruf2.toUpperCase())
